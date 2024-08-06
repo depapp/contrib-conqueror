89f22f2982c4446797c336151c4189aee0971b71
depapp <depa.article@gmail.com>
2024-08-06 14:43:11 +0700

    add umami tracking

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,8 +3,14 @@ import "@/styles/globals.css";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
+    <>
+    <Head>
+      <link href="/favicon.ico" type="image/png" />
+      <script defer src={`https://eu.umami.is/script.js`} data-website-id={process.env.UMAMI_WEBSITE_ID} crossorigin="anonymous"></script>
+    </Head>
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
+    </>
   );
 }
