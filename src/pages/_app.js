6d260e3c8efd54652ae4bc547132ab2eb77beaa7
depapp <depa.article@gmail.com>
2024-08-08 09:17:48 +0700

    integrate posthog analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,18 @@
 import { SessionProvider } from "next-auth/react";
 import "@/styles/globals.css";
 import Head from "next/head";
+import posthog from "posthog-js";
+import { PostHogProvider } from 'posthog-js/react';
+
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+    person_profiles: 'identified_only',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug();
+    },
+  });
+}
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
@@ -11,7 +23,9 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
         <script defer src={`https://eu.umami.is/script.js`} data-website-id={process.env.UMAMI_WEBSITE_ID} crossorigin="anonymous"></script>
       </Head>
       <SessionProvider session={session}>
-        <Component {...pageProps} />
+        <PostHogProvider client={posthog}>
+          <Component {...pageProps} />
+        </PostHogProvider>
       </SessionProvider>
     </>
   );
